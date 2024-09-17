bf1b8358ba5f3883dfe4c9d653d95dffc96bfedc
Ashrith Sagar <ashrithy@iisc.ac.in>
2024-09-17 06:10:25 +0530

    P1;
    load_train(), load_train_tf();

--- Projects/Project-1/Codes/utils/dataset.py
+++ Projects/Project-1/Codes/utils/dataset.py
@@ -4,7 +4,9 @@ dataset.py
 
 import os
 
+import numpy as np
 import tensorflow as tf
+from PIL import Image
 
 
 class GSVDataLoader:
@@ -16,10 +18,28 @@ class GSVDataLoader:
         tf.random.set_seed(seed)
         os.environ["TF_ENABLE_ONEDNN_OPTS"] = "0"
 
-    def load_train(self, batch_size=32):
+    def load_train_tf(self, batch_size=32):
         self.train_ds = tf.keras.preprocessing.image_dataset_from_directory(
             self.train_dir,
             label_mode="int",
             image_size=self.image_dims,
             batch_size=batch_size,
         )
+
+    def load_train(self):
+        train_ds = []
+        labels = []
+        label_map = {"A": 1, "B": 2, "C": 3, "D": 4, "S": 5}
+        for label in os.listdir(self.train_dir):
+            if not os.path.isdir(os.path.join(self.train_dir, label)):
+                continue
+            for img in os.listdir(os.path.join(self.train_dir, label)):
+                img_path = os.path.join(self.train_dir, label, img)
+                if not img_path.endswith(".jpg"):
+                    continue
+                image = Image.open(img_path)
+                image = np.array(image)
+                train_ds.append(image)
+                labels.append(label_map[label])
+        self.train_ds = np.array(train_ds)
+        self.labels = np.array(labels)
