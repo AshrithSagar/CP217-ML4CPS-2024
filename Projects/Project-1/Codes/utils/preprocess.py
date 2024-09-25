fe6ada8a1fec15fa296faab49f131053d290110a
Ashrith Sagar <ashrithy@iisc.ac.in>
2024-09-26 04:31:35 +0530

    P1

--- Projects/Project-1/Codes/utils/preprocess.py
+++ Projects/Project-1/Codes/utils/preprocess.py
@@ -4,21 +4,32 @@ preprocess.py
 
 import cv2
 import numpy as np
+import tensorflow as tf
 
 
-def flatten_preprocess(images):
-    num_samples = images.shape[0]
-    image_size = np.prod(images.shape[1:])
-    flattened_images = images.reshape(num_samples, image_size)
-    normalized_images = flattened_images / 255.0
-    return normalized_images
-
-
-def sift_preprocess(images):
+def extract_sift_features(image):
     sift = cv2.SIFT_create()
-    sift_features = []
-    for image in images:
-        _, des = sift.detectAndCompute(image, None)
-        feats = np.mean(des, axis=0) if des is not None else np.zeros(128)
-        sift_features.append(feats)
-    return np.array(sift_features)
+    _, des = sift.detectAndCompute(image, None)
+    return np.mean(des, axis=0) if des is not None else np.zeros(128)
+
+
+def sift_preprocess_sklearn(images):
+    features = [extract_sift_features(image) for image in images]
+    return np.array(features)
+
+
+def flatten_preprocess_sklearn(images):
+    normalized_images = images.astype(np.float32) / 255.0
+    return normalized_images.reshape(images.shape[0], -1)
+
+
+def sift_preprocess_tf(images):
+    sift_features = tf.map_fn(
+        lambda x: tf.numpy_function(extract_sift_features, [x], tf.float32), images
+    )
+    return sift_features
+
+
+def flatten_preprocess_tf(images):
+    normalized_images = tf.cast(images, tf.float32) / 255.0
+    return tf.reshape(normalized_images, (tf.shape(images)[0], -1))
